2aced94e1ec911e2266dea29043a7d5484619585
arunavabanerjee <mrarunbanerjee09@gmail.com>
2019-06-26 06:10:50 -0700

    Create pos_products_wholesale_price.lang.php

---
+++ pos_products_wholesale_price.lang.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'POS Products: WholesalePrice & Totals');
+
+define('TABLE_HEADING_PRODUCT_NUMBER', 'ProductNo.');
+define('TABLE_HEADING_PRODUCT_NAME', 'Product Name');
+define('TABLE_HEADING_PRODUCT_MODEL', 'Product Model'); 
+define('TABLE_HEADING_PRODUCT_QTY', 'Total Quantity'); 
+define('TABLE_HEADING_PRODUCT_WSPRICE', 'Price(Excl. Tax)'); 
+define('TABLE_HEADING_PRODUCT_ATTRIBUTES', 'Attributes');
+define('TABLE_HEADING_PRODUCT_ROW_TOTAL', 'Price X Quantity(Excl. Tax)');
+
+define('TABLE_HEADING_PRODUCT_ATTR_NAME', 'Attrib. Name');
+define('TABLE_HEADING_PRODUCT_ATTR_QTY', 'Attrib. Quantity');
+define('TABLE_HEADING_PRODUCT_WSPRICE', 'Attrib. Price');
+
+define('TABLE_HEADING_PURCHASED', 'Purchased');
+?>
